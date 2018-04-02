a29dc1d80a33c36aa48c6bbe05e6769371afbea9
David Silva <dsilva23@yahoo.com>
2018-04-02 23:12:03 +0000

    add .env variables

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ router.get("/register", function(req, res) {
 // handle sign up logic
 router.post("/register", function(req, res) {
    var newUser = new User({username: req.body.username});
-   if(req.body.adminCode === "secretcode123") {
+   if(req.body.adminCode === process.env.ADMIN_PASSWORD) {
       newUser.isAdmin = true;
    }
    User.register(newUser, req.body.password, function(err, user) {
