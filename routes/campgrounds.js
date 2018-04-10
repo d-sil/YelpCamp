ee840029dafe6bcc50f9256cf34cbf5f2266c766
David Silva <dsilva23@yahoo.com>
2018-04-10 17:59:47 +0000

    added search and pagination

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -13,15 +13,50 @@ var geocoder = NodeGeocoder(options);
 var Comment    = require("../models/comment");
 
 // INDEX Route - show all campgrounds
-router.get("/", function(req, res) {
-    // Get all campgrounds from DB
-    Campground.find({}, function(err, allCampgrounds) {
-       if(err) {
-           console.log(err);
-       } else {
-           res.render("campgrounds/index", {campgrounds: allCampgrounds, page: 'campgrounds'});
-       }
-    });
+router.get("/", function(req, res){
+    var perPage = 8;
+    var pageQuery = parseInt(req.query.page);
+    var pageNumber = pageQuery ? pageQuery : 1;
+    var noMatch = null;
+    if(req.query.search) {
+        const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+        Campground.find({name: regex}).skip((perPage * pageNumber) - perPage).limit(perPage).exec(function (err, allCampgrounds) {
+            Campground.count({name: regex}).exec(function (err, count) {
+                if (err) {
+                    console.log(err);
+                    res.redirect("back");
+                } else {
+                    if(allCampgrounds.length < 1) {
+                        noMatch = "No campgrounds match that query, please try again.";
+                    }
+                    res.render("campgrounds/index", {
+                        campgrounds: allCampgrounds,
+                        current: pageNumber,
+                        pages: Math.ceil(count / perPage),
+                        noMatch: noMatch,
+                        search: req.query.search
+                    });
+                }
+            });
+        });
+    } else {
+        // get all campgrounds from DB
+        Campground.find({}).skip((perPage * pageNumber) - perPage).limit(perPage).exec(function (err, allCampgrounds) {
+            Campground.count().exec(function (err, count) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.render("campgrounds/index", {
+                        campgrounds: allCampgrounds,
+                        current: pageNumber,
+                        pages: Math.ceil(count / perPage),
+                        noMatch: noMatch,
+                        search: false
+                    });
+                }
+            });
+        });
+    }
 });
 
 // CREATE Route - add new campground to DB
@@ -138,5 +173,9 @@ router.delete("/:id", middleware.isLoggedIn, middleware.checkUserCampground, fun
       }
     });
 });
+
+function escapeRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
 
 module.exports = router;  // we are exporting the routes
